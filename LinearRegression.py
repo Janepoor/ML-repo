600f802d5f89a44ff4c8618e74531d7aef489438
Janepoor <fjmjp@126.com>
2017-03-30 12:59:33 -0400

    Solve problem 2

---
+++ LinearRegression.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+from sklearn.preprocessing import scale
+from mpl_toolkits.mplot3d import Axes3D
+
+import matplotlib.pyplot as plt
+
+def compute_cost(X,y,b_):
+    return sum(np.array((np.matrix(X)*np.matrix(b_))-np.matrix(y).transpose())**2)/(2*np.array(y).size)
+
+def gradient_descent(X,y,b_,alpha):
+
+    t = (np.matrix(X)*np.matrix(b_))-np.matrix(y).transpose()
+    grad = alpha* (((np.matrix(X).transpose())*t)/np.array(y).size)
+    return b_ - grad
+
+
+def main():
+    inputfile = sys.argv[1]
+    outputfile = sys.argv[2]
+
+    lr = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10]
+    n_iter = 100
+
+    raw_data = np.loadtxt(inputfile, delimiter=',')
+    l, w = raw_data.shape
+    y = raw_data[0:l, w - 1]
+    X = raw_data[0:l, 0:w - 1]
+
+    age = scale(raw_data[:, [0]])
+    weight = scale(raw_data[:, [1]])
+    heights = scale(raw_data[:, [2]]).flatten()
+
+    X[:, [0]] = age
+    X[:, [1]] = weight
+    X = pd.concat([pd.DataFrame(np.ones([len(X), 1])), pd.DataFrame(X)], axis=1)
+
+    #print (X)
+
+    with open(outputfile, "w") as f:
+        for a in lr:
+            costhist = np.zeros(n_iter)
+            b_ = np.zeros([3, 1])
+            for i in range(n_iter):
+                b_ = gradient_descent(X, y, b_, a)
+                #costhist[i] = compute_cost(X, y, b_)
+            f.write("{},{},{},{},{}\n".format(a, n_iter, b_[0, 0], b_[1, 0], b_[2, 0]))
+
+            # plt.plot(costhist)
+            # plt.show()
+
+        my_eta = 0.6
+        my_iter = 80
+        # print("eta : {}".format(my_eta))
+        #costhist = np.zeros(my_iter)
+        b_ = np.zeros([3, 1])
+        for i in range(my_iter):
+            b_ = gradient_descent(X, y, b_, my_eta)
+            #costhist[i] = compute_cost(X, y, b_)
+        f.write("{},{},{},{},{}\n".format(my_eta, my_iter, b_[0, 0], b_[1, 0], b_[2, 0]))
+        #plt.plot(costhist)
+
+        fig =plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(raw_data[:, [0]],raw_data[:, [1]],raw_data[:, [2]])
+        x,y= np.meshgrid(np.arange(-3,3),np.arange(-2,4))
+        z= b_[0, 0] +b_[1, 0]*x +b_[2, 0]*y
+        # z= 0.0002 +0.8767*x +0.0093*y
+        ax.plot_surface(x,y,z)
+
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ problem2.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+class LinearRegressor:
+    def __init__(self, num_features=2, iterations=100, alpha=0.0001, of=None):
+        self.out = of
+        self.num_features = num_features + 1  # remember to include the intercept...
+        self.alpha = alpha
+        self.iterations = iterations
+        self.weights = np.zeros(self.num_features)
+
+    def predict(self, data):  # given alpha row, what is the predicted value
+        return np.dot(data, self.weights)
+
+    def fit(self, data, expectations):
+        e = 0.000001  # minimum change needed to declare convergence
+        for i in range(self.iterations):
+            v = self.weights.deepcopy()
+            self.fit_incremental_gradient_descent(data, expectations)
+            total_adj = sum([abs(wi - vi) for wi, vi in zip(self.weights, v)])
+            if total_adj < e:
+                break
+        self.out.write("%0.4f, %d, %0.4f, %0.4f, %0.4f\n" % (self.alpha, self.iterations, self.weights[0], self.weights[1], self.weights[2]))
+
+    def fit_incremental_gradient_descent(self, X: np.ndarray, Y: np.ndarray):
+        w = self.weights;
+        h = self.predict  # abbreviations
+        n = w.shape[0]
+        for i, x_i in enumerate(X):  # for each x_i in X (the training data set)
+            for j in range(n):  # for each feature in the test data instance, x_i,j
+                w[j] -= self.adj_weight(Y[i], h(x_i), x_i[j], self.alpha)
+
+    @staticmethod
+    def adj_weight(y, hx, xij, a):
+        # get a proportional fraction of the feature and remove from the corresponding weight
+        try:
+            return a * xij * (hx - y)
+        except OverflowError:
+            return 0.0
